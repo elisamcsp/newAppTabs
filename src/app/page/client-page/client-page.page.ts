730aa128d97f59213202ec42d88b54ef61ce0a1a
Elisa Mendez <emendezcaceres@gmail.com>
2022-03-12 21:50:16 +0100

    add image url for avatar

--- src/app/page/client-page/client-page.page.ts
+++ src/app/page/client-page/client-page.page.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ClientPagePage implements OnInit {
 
   clients: any[] = [
-    {"id": "1", "name": "Cliente 1", "dni": "DNI1", "address": "address 1", "avatar": "../../../assets/avatar-ben.png"},
-    {"id": "2", "name": "Cliente 2", "dni": "DNI2", "address": "address 2", "avatar": "../../../assets/avatar-finn.png"},
-    {"id": "3", "name": "Cliente 3", "dni": "DNI3", "address": "address 3", "avatar": "../../../assets/avatar-han.png"},
-    {"id": "4", "name": "Cliente 4", "dni": "DNI4", "address": "address 4", "avatar": "../../../assets/avatar-yoda.png"},
+    {"id": "1", "name": "Cliente 1", "dni": "DNI1", "address": "address 1", "avatar": "https://raw.githubusercontent.com/ionic-team/ionic-docs/main/static/demos/api/list/avatar-ben.png"},
+    {"id": "2", "name": "Cliente 2", "dni": "DNI2", "address": "address 2", "avatar": "https://raw.githubusercontent.com/ionic-team/ionic-docs/main/static/demos/api/list/avatar-rey.png"},
+    {"id": "3", "name": "Cliente 3", "dni": "DNI3", "address": "address 3", "avatar": "https://raw.githubusercontent.com/ionic-team/ionic-docs/main/static/demos/api/list/avatar-poe.png"},
+    {"id": "4", "name": "Cliente 4", "dni": "DNI4", "address": "address 4", "avatar": "https://raw.githubusercontent.com/ionic-team/ionic-docs/main/static/demos/api/list/avatar-leia.png"},
   ]; 
 
   constructor() { }
