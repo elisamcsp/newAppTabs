44cf0642cde8a9a3ceefdcf4001fc799ab753fd5
Elisa Mendez <emendezcaceres@gmail.com>
2022-03-17 22:01:26 +0100

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,24 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'client-page',
-    loadChildren: () => import('./page/client-page/client-page.module').then( m => m.ClientPagePageModule)
-  },
-  {
-    path: 'reservation-page',
-    loadChildren: () => import('./page/reservation-page/reservation-page.module').then( m => m.ReservationPagePageModule)
-  },
-  {
-    path: 'product-page',
-    loadChildren: () => import('./page/products-page/products-page.module').then( m => m.ProductsPagePageModule)
-  },  {
-    path: 'client-edit-page',
-    loadChildren: () => import('./page/client-edit-page/client-edit-page.module').then( m => m.ClientEditPagePageModule)
-  }
-
-
+  }  
 ];
 @NgModule({
   imports: [
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,28 +8,44 @@ const routes: Routes = [
     component: TabsPage,
     children: [      
       {
-        path: 'tab1',
+        path: 'client',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'reservation',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'product',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/client',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab2',
+    redirectTo: '/tabs/reservation',
     pathMatch: 'full'
+  },
+  {
+    path: 'clients',
+    loadChildren: () => import('../page/client-page/client-page.module').then( m => m.ClientPagePageModule)
+  },
+  {
+    path: 'reservations',
+    loadChildren: () => import('../page/reservation-page/reservation-page.module').then( m => m.ReservationPagePageModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('../page/products-page/products-page.module').then( m => m.ProductsPagePageModule)
+  },
+  {
+    path: 'client/edit',
+    loadChildren: () => import('../page/client-edit-page/client-edit-page.module').then( m => m.ClientEditPagePageModule)
   }
 ];
 
